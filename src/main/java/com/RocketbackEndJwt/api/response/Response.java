e7347bb904a93c4c5609bc862edaea79207b2fb0
Juan F Vásquez E <juanfvasquez267@gmail.com>
2019-03-31 13:20:32 -0500

    Adaptacion para una respuesta generica

--- src/main/java/com/RocketbackEndJwt/api/response/Response.java
+++ src/main/java/com/RocketbackEndJwt/api/response/Response.java
@@ -1,29 +1,37 @@
 package com.RocketbackEndJwt.api.response;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Response<T> {
+public class Response<T> implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 	
 	private T data;
-	private List<String> errors;
+	private List<T> list;
+	private String message;
+	private int code;
 	
-	public T getData() {
-		return data;
-	}
-	
-	public void setData(T data) {
+	public Response(T data, String message, int code) {
 		this.data = data;
+		this.list = new ArrayList<>();
+		this.message = message;
+		this.code = code;
 	}
 	
-	public List<String> getErrors(){
-		if(this.errors == null) {
-			this.errors = new ArrayList<String>();
-		}
-		return errors;
+	public Response(List<T> list, String message, int code) {
+		this.list = list;
+		this.data = (T) new Object();
+		this.message = message;
+		this.code = code;
 	}
 	
-	public void setErrors(List<String> errors) {
-		this.errors = errors;
+	public Response(String message, int code) {
+		this.data = (T) new Object();
+		this.list = new ArrayList<>();
+		this.message = message;
+		this.code = code;
 	}
+	
 }
