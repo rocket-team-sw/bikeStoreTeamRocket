bb282eea6216c23c108de41fad59fa8114625145
Juan F Vásquez E <juanfvasquez267@gmail.com>
2019-03-31 15:32:25 -0500

    Eliminacion de relaciones explicitas por conflictos en Compras

--- src/main/java/com/RocketbackEndJwt/api/entities/Compra.java
+++ src/main/java/com/RocketbackEndJwt/api/entities/Compra.java
@@ -9,7 +9,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.TableGenerator;
 
