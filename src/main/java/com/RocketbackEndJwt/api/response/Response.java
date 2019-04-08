92039d53beb25cb95f42029d1580e697a74234af
Juan F Vásquez E <juanfvasquez267@gmail.com>
2019-04-08 07:46:47 -0500

    Se organiza la respuesta generica

--- src/main/java/com/RocketbackEndJwt/api/response/Response.java
+++ src/main/java/com/RocketbackEndJwt/api/response/Response.java
@@ -1,37 +1,47 @@
 package com.RocketbackEndJwt.api.response;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Response<T> implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+private static final long serialVersionUID = 1L;
 	
-	private T data;
-	private List<T> list;
-	private String message;
-	private int code;
+	T data;
+	List<T> list;
+	String message;
+	int code;
 	
-	public Response(T data, String message, int code) {
+	public Response(T data, List<T> list, String message, int code) {
+		super();
 		this.data = data;
-		this.list = new ArrayList<>();
-		this.message = message;
-		this.code = code;
-	}
-	
-	public Response(List<T> list, String message, int code) {
 		this.list = list;
-		this.data = (T) new Object();
 		this.message = message;
 		this.code = code;
 	}
 	
-	public Response(String message, int code) {
-		this.data = (T) new Object();
-		this.list = new ArrayList<>();
-		this.message = message;
-		this.code = code;
+	public T getData() {
+		return data;
 	}
-	
+	public void setData(T data) {
+		this.data = data;
+	}
+	public List<T> getList() {
+		return list;
+	}
+	public void setList(List<T> list) {
+		this.list = list;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}	
 }
