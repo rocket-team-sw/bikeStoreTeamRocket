af086d24f6065940f78305f00642e566a819bf7a
Juan F Vásquez E <juanfvasquez267@gmail.com>
2019-04-26 09:47:06 -0500

    SE modifica controlador para recibir parametros por url y validaciones

--- src/main/java/com/RocketbackEndJwt/api/controller/ModeloController.java
+++ src/main/java/com/RocketbackEndJwt/api/controller/ModeloController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.RocketbackEndJwt.api.entities.Marca;
 import com.RocketbackEndJwt.api.entities.Modelo;
 import com.RocketbackEndJwt.api.response.Response;
 import com.RocketbackEndJwt.api.service.ModeloDAO;
@@ -38,12 +39,12 @@ public class ModeloController {
 	public @ResponseBody ResponseEntity<Response<Modelo>> create(@RequestBody Modelo modelo, HttpServletRequest request,
 			HttpServletResponse response) {
 		try {
-			// TODO: Validate data
+			validate(modelo);
 			dao.save(modelo);
 			respuesta = new Response<>(modelo, emptyList, "Ok", 200);
 			return new ResponseEntity<Response<Modelo>>(respuesta, HttpStatus.OK);
 		} catch (Exception e) {
-			respuesta = new Response<>(emptyObject, emptyList, "Error creando modelos", 400);
+			respuesta = new Response<>(emptyObject, emptyList, "Error creando modelos. " + e.getMessage(), 400);
 			return new ResponseEntity<Response<Modelo>>(respuesta, HttpStatus.BAD_REQUEST);
 		}
 	}
@@ -65,11 +66,17 @@ public class ModeloController {
 	public @ResponseBody ResponseEntity<Response<Modelo>> get(@PathVariable Long id, HttpServletRequest request,
 			HttpServletResponse response) {
 		try {
+			if (id == null) {
+				error("Debe ingresar un id válido");
+			}
 			Modelo modelo = dao.findById(id).get();
+			if (modelo == null) {
+				error("El id no esta registrado");
+			}
 			respuesta = new Response<>(modelo, emptyList, "Ok", 200);
 			return new ResponseEntity<Response<Modelo>>(respuesta, HttpStatus.OK);
 		} catch (Exception e) {
-			respuesta = new Response<>(emptyObject, emptyList, "Error Leyendo modelo", 400);
+			respuesta = new Response<>(emptyObject, emptyList, "Error Leyendo modelo. " + e.getMessage(), 400);
 			return new ResponseEntity<Response<Modelo>>(respuesta, HttpStatus.BAD_REQUEST);
 		}
 	}
@@ -78,7 +85,14 @@ public class ModeloController {
 	public @ResponseBody ResponseEntity<Response<Modelo>> update(@PathVariable Long id, @RequestBody Modelo newModelo, HttpServletRequest request,
 			HttpServletResponse response) {
 		try {
+			if (id == null) {
+				error("Debe ingresar un id válido");
+			}
 			Modelo modelo = dao.findById(id).get();
+			if (modelo == null) {
+				error("El id no esta registrado");
+			}
+			validate(newModelo);
 			modelo.setNombre(newModelo.getNombre());
 			modelo.setDescripcion(newModelo.getDescripcion());
 			modelo.setFecha_borrado(newModelo.getFecha_borrado());
@@ -86,7 +100,7 @@ public class ModeloController {
 			respuesta = new Response<>(emptyObject, emptyList, "Ok", 200);
 			return new ResponseEntity<Response<Modelo>>(respuesta, HttpStatus.OK);
 		} catch (Exception e) {
-			respuesta = new Response<>(emptyObject, emptyList, "Error actualizando modelo", 400);
+			respuesta = new Response<>(emptyObject, emptyList, "Error actualizando modelo. " + e.getMessage(), 400);
 			return new ResponseEntity<Response<Modelo>>(respuesta, HttpStatus.BAD_REQUEST);
 		}
 	}
@@ -95,13 +109,42 @@ public class ModeloController {
 	public @ResponseBody ResponseEntity<Response<Modelo>> delete(@PathVariable Long id, HttpServletRequest request,
 			HttpServletResponse response) {
 		try {
+			if (id == null) {
+				error("Debe ingresar un id válido");
+			}
 			Modelo modelo = dao.findById(id).get();
+			if (modelo == null) {
+				error("El id no esta registrado");
+			}
 			dao.delete(modelo);
 			respuesta = new Response<>(emptyObject, emptyList, "Ok", 200);
 			return new ResponseEntity<Response<Modelo>>(respuesta, HttpStatus.OK);
 		} catch (Exception e) {
-			respuesta = new Response<>(emptyObject, emptyList, "Error borrando modelo", 400);
+			respuesta = new Response<>(emptyObject, emptyList, "Error borrando modelo. " + e.getMessage(), 400);
 			return new ResponseEntity<Response<Modelo>>(respuesta, HttpStatus.BAD_REQUEST);
 		}
+	}
+	
+private void validate(Modelo modelo) throws Exception {
+		
+		if (modelo.getNombre() == null) {
+			error("Debe ingresar un nombre");
+		}
+		
+		if (modelo.getNombre().length() < 5) {
+			error("DEbe ingresar un nombre mas largo");
+		}
+		
+		if (modelo.getDescripcion() == null) {
+			error("Debe ingresar una descripcion");
+		}
+		
+		if (modelo.getDescripcion().length() < 10) {
+			error("Debe ingresar una descripcion mas larga");
+		}
+	}
+	
+	private void error(String msg) throws Exception {
+		throw new Exception(msg);
 	}
 }
