b82c70444a5779d984378a4cae4408b567e18117
Juan F Vásquez E <juanfvasquez267@gmail.com>
2019-04-26 09:44:10 -0500

    SE modifica controlador para recibir parametros por url y validaciones

--- src/main/java/com/RocketbackEndJwt/api/controller/CategoriaController.java
+++ src/main/java/com/RocketbackEndJwt/api/controller/CategoriaController.java
@@ -38,12 +38,12 @@ public class CategoriaController {
 	public @ResponseBody ResponseEntity<Response<Categoria>> create(@RequestBody Categoria categoria, HttpServletRequest request,
 			HttpServletResponse response) {
 		try {
-			// TODO: Validate data
+			validate(categoria);
 			dao.save(categoria);
 			respuesta = new Response<>(categoria,emptyList, "Ok", 200);
 			return new ResponseEntity<Response<Categoria>>(respuesta, HttpStatus.OK);
 		} catch (Exception e) {
-			respuesta = new Response<>(emptyObject, emptyList, "Error creando categorias", 400);
+			respuesta = new Response<>(emptyObject, emptyList, "Error creando categorias. " + e.getMessage(), 400);
 			return new ResponseEntity<Response<Categoria>>(respuesta, HttpStatus.BAD_REQUEST);
 		}
 	}
@@ -65,11 +65,17 @@ public class CategoriaController {
 	public @ResponseBody ResponseEntity<Response<Categoria>> get(@PathVariable Long id, HttpServletRequest request,
 			HttpServletResponse response) {
 		try {
+			if (id == null) {
+				error("Debe ingresar un id válido");
+			}
 			Categoria categoria = dao.findById(id).get();
+			if (categoria == null) {
+				error("El id no esta registrado");
+			}
 			respuesta = new Response<>(categoria, emptyList, "Ok", 200);
 			return new ResponseEntity<Response<Categoria>>(respuesta, HttpStatus.OK);
 		} catch (Exception e) {
-			respuesta = new Response<>(emptyObject, emptyList, "Error Leyendo categoria", 400);
+			respuesta = new Response<>(emptyObject, emptyList, "Error Leyendo categoria." + e.getMessage(), 400);
 			return new ResponseEntity<Response<Categoria>>(respuesta, HttpStatus.BAD_REQUEST);
 		}
 	}
@@ -78,7 +84,14 @@ public class CategoriaController {
 	public @ResponseBody ResponseEntity<Response<Categoria>> update(@PathVariable Long id, @RequestBody Categoria newCategoria, HttpServletRequest request,
 			HttpServletResponse response) {
 		try {
+			if (id == null) {
+				error("Debe ingresar un id válido");
+			}
 			Categoria categoria = dao.findById(id).get();
+			if (categoria == null) {
+				error("El id no esta registrado");
+			}
+			validate(newCategoria);
 			categoria.setDescripcion(newCategoria.getDescripcion());
 			categoria.setFecha_borrado(newCategoria.getFecha_borrado());
 			categoria.setNombre(newCategoria.getNombre());
@@ -86,7 +99,7 @@ public class CategoriaController {
 			respuesta = new Response<>(emptyObject, emptyList, "Ok", 200);
 			return new ResponseEntity<Response<Categoria>>(respuesta, HttpStatus.OK);
 		} catch (Exception e) {
-			respuesta = new Response<>(emptyObject, emptyList, "Error actualizando categoria", 400);
+			respuesta = new Response<>(emptyObject, emptyList, "Error actualizando categoria." + e.getMessage(), 400);
 			return new ResponseEntity<Response<Categoria>>(respuesta, HttpStatus.BAD_REQUEST);
 		}
 	}
@@ -95,7 +108,13 @@ public class CategoriaController {
 	public @ResponseBody ResponseEntity<Response<Categoria>> delete(@PathVariable Long id, HttpServletRequest request,
 			HttpServletResponse response) {
 		try {
+			if (id == null) {
+				error("Debe ingresar un id válido");
+			}
 			Categoria categoria = dao.findById(id).get();
+			if (categoria == null) {
+				error("El id no esta registrado");
+			}
 			dao.delete(categoria);
 			respuesta = new Response<>(emptyObject, emptyList, "Ok", 200);
 			return new ResponseEntity<Response<Categoria>>(respuesta, HttpStatus.OK);
@@ -103,5 +122,28 @@ public class CategoriaController {
 			respuesta = new Response<>(emptyObject, emptyList, "Error borrando categoria", 400);
 			return new ResponseEntity<Response<Categoria>>(respuesta, HttpStatus.BAD_REQUEST);
 		}
+	}
+	
+	private void validate(Categoria categoria) throws Exception {
+		
+		if (categoria.getNombre() == null) {
+			error("Debe ingresar un nombre");
+		}
+		
+		if (categoria.getNombre().length() < 5) {
+			error("DEbe ingresar un nombre mas largo");
+		}
+		
+		if (categoria.getDescripcion() == null) {
+			error("Debe ingresar una descripcion");
+		}
+		
+		if (categoria.getDescripcion().length() < 10) {
+			error("Debe ingresar una descripcion mas larga");
+		}
+	}
+	
+	private void error(String msg) throws Exception {
+		throw new Exception(msg);
 	}
 }
