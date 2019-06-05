708ba18023a5011259176e1c14e3ae120fd63b9d
Juan F Vásquez E <juanfvasquez267@gmail.com>
2019-06-04 19:40:52 -0500

    Se crea archivo para contener los codigos de respuesta

---
+++ src/main/java/com/RocketbackEndJwt/api/response/ResponseCode.java
@@ -0,0 +1,12 @@
+package com.RocketbackEndJwt.api.response;
+
+/**
+ * Clase que va a contener los códigos de mensajes 
+ * @author juanfvasquez
+ * 04/JUN/2019
+ */
+public class ResponseCode {
+	
+	public final static int OK_CODE = 200;
+	public final static int ERROR_CODE = 200;
+}
